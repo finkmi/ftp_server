5e406782e4da0899068f8d6e7bb445e5157d169e
finkmi <finkmi@mail.gvsu.edu>
2021-03-13 11:02:43 -0500

    Added the connect command (untested with server) and added .gitignore

--- client.c
+++ client.c
@@ -14,7 +14,16 @@ int quitflag = 0;
 
 int main() {
 
-	char *buffer, *command;
+	//used to read in and parse user commands
+	char buffer[BUFFER_SIZE];
+	char *command;
+	
+	//used for connecting socket
+	struct sockaddr_in serv_addr;
+    	struct hostent *server;
+	char *ip, *tempno;
+	int sockfd, portno;
+
 	printf("Client starting...\n");
 
 	while(!quitflag) {
@@ -36,25 +45,68 @@ int main() {
 			command[i] = tolower(command[i]);
 		}
 
-		//Determine what input was given by user
-		if(!(strcmp(buffer, "connect\n"))) {
-
-		}
-		else if(!(strcmp(buffer, "list\n"))) {
-
-		}
-		else if(!(strcmp(buffer, "retrieve\n"))) {
-
-		}
-		else if(!(strcmp(buffer, "store\n"))) {
-
-		}
-		else if(!(strcmp(buffer, "quit\n"))) {
+		//Determine what input was given by user and execute command
+		
+		printf("tokenized command: %s\n", command);
+		//Connect command
+		if(!(strcmp(command, "connect"))) {
 			
+			//Get ip and port number from user input checking for errors
+			if((ip = strtok(buffer, " ")) == NULL) {
+				printf("Error tokenizing server ip please enter -> CONNECT <ip/name> <server port>\n");
+				continue;
+			}
+			if((tempno = strtok(buffer, " ")) == NULL) {
+				printf("Error tokenizing port number please enter -> CONNECT <ip/name> <server port>\n");
+				continue;
+			}
+			portno = atoi(tempno);
+
+			//Open socket and gethostname checking for errors
+			if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+				printf("Error opening socket\n");
+				continue;
+			}
+			if((server = gethostbyname(ip)) == NULL) {
+				printf("Error: No such host\n");
+				continue;
+			}
+
+			//Attempt to connect socket
+			bzero((char *) &serv_addr, sizeof(serv_addr));
+    			serv_addr.sin_family = AF_INET;
+    			bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+   			serv_addr.sin_port = htons(portno);
+
+			printf("here\n");
+
+			if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) {
+       				printf("Error connecting socket\n");
+				continue;
+			}
+
+			printf("testing connect\n");
+
+
+		}
+
+		else if(!(strcmp(command, "list\n"))) {
+			printf("testing list\n");
+		}
+		else if(!(strcmp(command, "retrieve"))) {
+			printf("testing retrieve\n");
+		}
+		else if(!(strcmp(command, "store"))) {
+			printf("testing store\n");
+		}
+		else if(!(strcmp(command, "quit\n"))) {
+			printf("testing quit\n");	
 			//close connection
 			quitflag = 1;
 		}
 		else {
+			//NOTE: I think that newline char only goes on list and quit as they have no other arg
+			//needs more testing
 			printf("Command not recognized please be sure to enter in command correctly\n");
 		}
 	}
